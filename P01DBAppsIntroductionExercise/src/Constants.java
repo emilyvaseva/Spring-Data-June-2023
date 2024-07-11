f94b5d0410dd79b4fb58c2a2ff50ece688ea9f2d
emilyvaseva <114073206+emilyvaseva@users.noreply.github.com>
2024-07-11 12:24:08 +0300

    add first tasks

---
+++ P01DBAppsIntroductionExercise/src/Constants.java
@@ -0,0 +1,9 @@
+enum Constants {
+    ;
+    static final String USER_KEY = "user";
+    static final String USER_VALUE = "root";
+    static final String PASSWORD_KEY = "password";
+    static final String PASSWORD_VALUE = "1234";
+    static final String JDBC_URL = "jdbc:mysql://localhost:3306/minions_db";
+    static final String COLUMN_LABEL_NAME = "name";
+}
